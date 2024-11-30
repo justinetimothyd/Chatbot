0610cf03ead2bf545c4407738414dc3bbd08e481
justinetimothyd <119463859+justinetimothyd@users.noreply.github.com>
2024-12-01 00:40:54 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
 
 // Toggle mobile menu
 menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
+
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+let runTimeOut;
+let runAutoRun= setTimeout(() => {
+    nextDom.click();
+}, timeAutoNext);
+
+nextDom.onclick = function(){
+    showSlider('next');
+}
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+    } else{
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carouselDom.classList.add('prev');
+    }
+    
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() =>{
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning)
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(() => {
+        nextDom.click();
+    }, timeAutoNext);
+}
+
+prevDom.addEventListener('click', function () {
+    
+    carouselDom.classList.remove('next');  // Ensure 'next' class is removed
+    
+    resetCarousel();  // Optionally reset the auto transition timer
 });
